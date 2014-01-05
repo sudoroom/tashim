a399800df90ed6b36f73fa5c25e3662821464dc2
yar <yardenack@gmail.com>
2014-01-04 17:16:26 -0800

    front shims!

---
+++ front/compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# probably only useful for me, sorry
+export PATH=/usr/bin:/usr/bin/core_perl:/usr/bin/vendor_perl
+
+# always execute in THIS dir
+self=$(readlink -e "$0") || exit 1
+self=$(dirname "${self}") || exit 1
+cd "$self"
+
+openscad -o front.stl front.scad
+
+slic3r \
+	 --layer-height 0.25 \
+	 --perimeters 1 \
+	 --solid-layers 1 \
+	 --nozzle-diameter 0.35 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 190,50 \
+	 --skirts 2 \
+	 --fill-density 0.1 \
+	 -o front.gcode front.stl
+
+sed -i 's/M104 S200/M104 S220/' front.gcode
+sed -i 's/M109 S200/M109 S220/' front.gcode
